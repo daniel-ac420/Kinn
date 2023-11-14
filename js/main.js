388768d2b085171a825860405d0aa689125dc378
daniel-ac420 <67051360+daniel-ac420@users.noreply.github.com>
2023-11-15 01:26:30 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+﻿/* --- MOBILE MENU --- */
+
+const mobileMenuButton = document.querySelector(".mobile-menu-button");
+
+const mobileMenu = document.querySelector(".mobile-menu");
+
+mobileMenuButton.addEventListener("click", function() {
+	if (!this.classList.contains("active")) {
+		this.classList.toggle("active");
+		mobileMenu.classList.toggle("active");
+	}
+})
+
+
+
+const closeButton = document.querySelector(".button-close");
+
+closeButton.addEventListener("click", function() {
+	if (mobileMenu.classList.contains("active")) {
+		mobileMenu.classList.toggle("active");
+		mobileMenuButton.classList.toggle("active");
+	}
+})
